6f7842526d68e15d7205798d7d0fb03219c003fc
Hypocisy <1137943609@qq.com>
2025-02-09 03:19:34 +0800

    - Remove doubled type wrappers

--- src/main/java/top/theillusivec4/champions/common/integration/kubejs/ChampionsKubeJSPlugin.java
+++ src/main/java/top/theillusivec4/champions/common/integration/kubejs/ChampionsKubeJSPlugin.java
@@ -1,20 +1,11 @@
 package top.theillusivec4.champions.common.integration.kubejs;
 
 import dev.latvian.mods.kubejs.KubeJSPlugin;
-import dev.latvian.mods.kubejs.script.ScriptType;
-import dev.latvian.mods.rhino.util.wrap.TypeWrappers;
-import net.minecraft.advancements.critereon.MinMaxBounds;
 import top.theillusivec4.champions.common.integration.kubejs.eventjs.ChampionsEvents;
-import top.theillusivec4.champions.common.integration.kubejs.wrapper.MinMaxBoundsIntsJS;
 
 public class ChampionsKubeJSPlugin extends KubeJSPlugin {
     @Override
     public void registerEvents() {
         ChampionsEvents.GROUP.register();
-    }
-
-    @Override
-    public void registerTypeWrappers(ScriptType type, TypeWrappers typeWrappers) {
-        typeWrappers.registerSimple(MinMaxBounds.Ints.class, MinMaxBoundsIntsJS::of);
     }
 }
