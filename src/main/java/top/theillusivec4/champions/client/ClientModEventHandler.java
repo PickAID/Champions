50f893093637cb3164eeb6089eb96a3c8a2aaa94
Hypocisy <1137943609@qq.com>
2025-06-20 02:11:18 +0800

    - Upgrade neo version - Fixed Adaptable affix sometime crashes server - Use DataComponents.ITEM_MODEL to override the ChampionEgg ItemStack Model - remove RegisterColorHandlersEvent.ItemTintSources event to register custom tints - change champion_egg.json that fixed give command champion egg hasn't default model - remove useless egg color source - remove useless mixin for access item tints - upgrade parchment version number and mapping number - 1.21.5-2.1.12.0 release

--- src/main/java/top/theillusivec4/champions/client/ClientModEventHandler.java
+++ src/main/java/top/theillusivec4/champions/client/ClientModEventHandler.java
@@ -4,13 +4,11 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
-import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
 import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 import top.theillusivec4.champions.Champions;
 import top.theillusivec4.champions.client.renderer.ColorizedBulletRenderer;
-import top.theillusivec4.champions.client.util.EggColorSource;
 import top.theillusivec4.champions.common.particle.RankParticle;
 import top.theillusivec4.champions.common.registry.ModEntityTypes;
 import top.theillusivec4.champions.common.registry.ModParticleTypes;
@@ -18,11 +16,6 @@ import top.theillusivec4.champions.common.util.Utils;
 
 @EventBusSubscriber(value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD, modid = Champions.MODID)
 public class ClientModEventHandler {
-
-  @SubscribeEvent
-  public static void onRegisterColor(final RegisterColorHandlersEvent.ItemTintSources event) {
-    event.register(Utils.getLocation("champion_egg"), EggColorSource.MAP_CODEC);
-  }
 
   @SubscribeEvent
   public static void registerGuiOverlayEvent(final RegisterGuiLayersEvent evt) {
