0ca21e6027a5c1017d9ee76be462881f5b5329c6
Hypocisy <1137943609@qq.com>
2025-06-20 23:38:16 +0800

    - Fixed jade plugin not render properly - 2.1.12.0 release

--- src/main/java/top/theillusivec4/champions/client/integration/jade/JadePlugin.java
+++ src/main/java/top/theillusivec4/champions/client/integration/jade/JadePlugin.java
@@ -10,20 +10,20 @@ import top.theillusivec4.champions.client.ChampionsOverlay;
 @WailaPlugin
 public class JadePlugin implements IWailaPlugin {
 
-    @Override
-    public void register(IWailaCommonRegistration registration) {
-        // register data provider
-    }
+	@Override
+	public void register(IWailaCommonRegistration registration) {
+		// register data provider
+	}
 
-    @Override
-    public void registerClient(IWailaClientRegistration registration) {
-        // register component providers, icon providers, callbacks, and config options here
-        registration.registerEntityComponent(ChampionComponentProvider.INSTANCE, LivingEntity.class);
-        registration.addBeforeRenderCallback(((iBoxElement, tooltipRect, guiGraphics, accessor, colorSetting) -> {
-            if (ChampionsOverlay.isRendering) {
-                tooltipRect.setY(tooltipRect.getY() + ChampionsOverlay.startY + 38);
-            }
-            return false;
-        }));
-    }
+	@Override
+	public void registerClient(IWailaClientRegistration registration) {
+		// register component providers, icon providers, callbacks, and config options here
+		registration.registerEntityComponent(ChampionComponentProvider.INSTANCE, LivingEntity.class);
+		registration.addBeforeRenderCallback(((iBoxElement, tooltipRect, guiGraphics, accessor, colorSetting) -> {
+			if (ChampionsOverlay.isRendering) {
+				tooltipRect.setY(ChampionsOverlay.startY + 38);
+			}
+			return false;
+		}));
+	}
 }
