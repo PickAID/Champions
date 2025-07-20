add23675e3338ecf9b068b874968c116409eb834
Hypocisy <1137943609@qq.com>
2025-07-20 17:31:21 +0800

    - Fix ConcurrentModificationException

--- src/main/java/top/theillusivec4/champions/common/affix/core/GoalAffix.java
+++ src/main/java/top/theillusivec4/champions/common/affix/core/GoalAffix.java
@@ -1,5 +1,6 @@
 package top.theillusivec4.champions.common.affix.core;
 
+import net.minecraft.server.TickTask;
 import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
@@ -8,24 +9,30 @@ import top.theillusivec4.champions.api.IChampion;
 import top.theillusivec4.champions.api.affix.IAffixLifecycle;
 
 import java.util.List;
+import java.util.Optional;
 
 public abstract class GoalAffix extends BasicAffix implements IAffixLifecycle {
 
-    @Override
-    public void onSpawn(IChampion champion) {
-        LivingEntity livingEntity = champion.getLivingEntity();
+	@Override
+	public void onSpawn(IChampion champion) {
+		LivingEntity livingEntity = champion.getLivingEntity();
 
-        if (livingEntity instanceof Mob mobEntity) {
-            this.getGoals(champion)
-                    .forEach(goal -> mobEntity.goalSelector.addGoal(goal.getA(), goal.getB()));
-        }
-    }
+		if (livingEntity instanceof Mob mobEntity) {
 
-    @Override
-    public boolean canApply(IChampion champion) {
-        LivingEntity livingEntity = champion.getLivingEntity();
-        return livingEntity instanceof Mob && super.canApply(champion);
-    }
+			// add goal after 10 tick(safely
+			var server = Optional.ofNullable(mobEntity.getServer());
+			server.ifPresent(mcServer -> mcServer.doRunTask(new TickTask(10, () ->
+					this.getGoals(champion)
+							.forEach(goal -> mobEntity.goalSelector.addGoal(goal.getA(), goal.getB()))
+			)));
+		}
+	}
 
-    public abstract List<Tuple<Integer, Goal>> getGoals(IChampion champion);
+	@Override
+	public boolean canApply(IChampion champion) {
+		LivingEntity livingEntity = champion.getLivingEntity();
+		return livingEntity instanceof Mob && super.canApply(champion);
+	}
+
+	public abstract List<Tuple<Integer, Goal>> getGoals(IChampion champion);
 }
