a8dafc7940037e6582896299a88407444752b86b
Hypocisy <1137943609@qq.com>
2025-09-19 02:06:06 +0800

    - Fixed `middle click champions can copy champions egg feature` crashes server when startup - upgrade project

--- src/main/java/top/theillusivec4/champions/common/mixin/ServerGamePacketListenerImplMixin.java
+++ src/main/java/top/theillusivec4/champions/common/mixin/ServerGamePacketListenerImplMixin.java
@@ -1,40 +1,53 @@
 package top.theillusivec4.champions.common.mixin;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.network.protocol.game.ServerboundPickItemFromEntityPacket;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
-import net.minecraft.util.Tuple;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.theillusivec4.champions.common.capability.ChampionAttachment;
+import top.theillusivec4.champions.common.rank.Rank;
 import top.theillusivec4.champions.common.util.ChampionHelper;
 import top.theillusivec4.champions.server.command.ChampionsCommand;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class ServerGamePacketListenerImplMixin {
-  @ModifyArg(
+  @Shadow
+  public ServerPlayer player;
+
+  @Shadow
+  protected abstract void tryPickItem(ItemStack stack);
+
+  @Inject(
     method = "handlePickItemFromEntity",
-    at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;tryPickItem(Lnet/minecraft/world/item/ItemStack;)V")
+    at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;tryPickItem(Lnet/minecraft/world/item/ItemStack;)V"),
+    cancellable = true
   )
-  private ItemStack modifyItemStack(ItemStack original) {
-    var pickedEntity = Minecraft.getInstance().crosshairPickEntity;
-    var player = Minecraft.getInstance().player;
-    var gameMode = Minecraft.getInstance().gameMode;
-    if (pickedEntity != null && player != null && gameMode != null) {
+  private void modifyItemStack(ServerboundPickItemFromEntityPacket pickItemFromEntityPacket, CallbackInfo ci) {
+    var level = this.player.level();
+    if (!(level instanceof ServerLevel serverLevel)) return;
+    var pickedEntity = serverLevel.getEntity(pickItemFromEntityPacket.id());
+    if (pickedEntity != null && this.player != null) {
       var championOptional = ChampionAttachment.getAttachment(pickedEntity);
       if (championOptional.isPresent()) {
         var champion = championOptional.get();
-        var clientChampion = champion.getClient();
+        var serverChampion = champion.getServer();
 
-        if (ChampionHelper.isValidChampion(clientChampion)) {
+        if (ChampionHelper.isValidChampion(serverChampion)) {
           var type = champion.getLivingEntity().getType();
-          var tier = clientChampion.getRank().map(Tuple::getA).orElseThrow();
-          var affixes = clientChampion.getAffixes();
-          return ChampionsCommand.createEgg(type, tier, affixes);
+          var tier = serverChampion.getRank().map(Rank::getTier).orElseThrow();
+          var affixes = serverChampion.getAffixes();
+          var createdEgg = ChampionsCommand.createEgg(type, tier, affixes);
+          // overwrite original tryPickItem invoke
+          this.tryPickItem(createdEgg);
+          ci.cancel();
         }
       }
     }
-    return original;
   }
 }
